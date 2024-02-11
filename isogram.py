b81b463300cfbc971a9eca5992e48b68b770d8a8
Omar Gonzalez <om.gonzalez007@gmail.com>
2024-02-11 10:37:06 -0500

    create functions

--- isogram.py
+++ isogram.py
@@ -1,25 +1,30 @@
+def count_word(word):
+    word_list = list(word)
+    dict_letter = {}
+    for i in word_list:
+        count = word_list.count(i)
+        dict_letter[i] = count
+        count = 0
+    print(dict_letter)
+    return dict_letter
+
+def word_isogram(word):
+    flag = True
+    dict_letter = count_word(word)
+    for i in dict_letter:
+        if dict_letter[i] > 1:
+            flag = False
+    return flag
+
 print("Determine if a word or phrase is an isogram")
 word = input("Input a word: ")
 
-word_list = list(word)
-dict_letter = {}
-flag = True
-
-for i in word_list:
-    count = word_list.count(i)
-    dict_letter[i] = count    
-    count = 0
-
-for i in dict_letter:
-    if dict_letter[i] > 1:
-        flag = False
-
+flag = word_isogram(word)
 
 if flag == True:
     print(f"Word {word} is isogram")
 else:
     print(f"word {word} is not isogram")
 
-print(dict_letter)
 
 
